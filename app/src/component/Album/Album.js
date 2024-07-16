526f03eb4c20c7ea80152eef36186a0378211300
Ksenia <kseniamakeko@gmail.com>
2024-07-16 18:05:28 +0500

    add get request for album songs

--- app/src/component/Album/Album.js
+++ app/src/component/Album/Album.js
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./Album.module.css";
-// import SongsList from "./SongsList";
+import SongsList from "./SongsList";
 
 const Album = ({ albumCard, onClick }) => {
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    const fetchSongs = async () => {
+      try {
+        const res = await fetch(
+          `http://localhost:5000/albums/${albumCard.id}/songs`
+        );
+        if (!res.ok) {
+          throw new Error("it is impossible to connect! Check your Network");
+        }
+        const data = await res.json();
+        setSongs(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchSongs();
+  }, [albumCard.id]);
   return (
-    <div className={classes.album_card}>
-      <button onClick={onClick}>Back</button>
-      <img src={albumCard.image_url} />
-      <h3>{albumCard.name}</h3>
-      <h4>{albumCard.authorName}</h4>
-      <p>{albumCard.createdAt}</p>
-      <p>{albumCard.description}</p>
-      {/* <div>
-        <SongsList />
-      </div> */}
+    <div className={classes.album_container}>
+      <div className={classes.album_left}>
+        <button onClick={onClick}>Back</button>
+        {albumCard.image_url && (
+          <img src={`http://localhost:5000/${albumCard.image_url}`} />
+        )}
+        <h3>{albumCard.name}</h3>
+        <h4>{albumCard.authorName}</h4>
+        <p>{new Date(albumCard.createdAt).getFullYear()}</p>
+        <p>{albumCard.description}</p>
+      </div>
+      <div className={classes.album_right}>
+        <div className={classes.album_right}>
+          <SongsList className={classes.songs_list} items={songs} />
+        </div>
+      </div>
     </div>
   );
 };
