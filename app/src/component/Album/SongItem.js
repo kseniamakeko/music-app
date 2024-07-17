8844845c9efd5347c10570d3c651770c4283719e
Ksenia <kseniamakeko@gmail.com>
2024-07-17 11:25:39 +0500

    add title and styles for songs

--- app/src/component/Album/SongItem.js
+++ app/src/component/Album/SongItem.js
@@ -3,14 +3,15 @@ import classes from "./SongItem.module.css";
 
 const SongItem = ({ id, name, duration }) => {
   return (
-    <div className={classes.album_item}>
-      <div className={classes.title} />
-      <div>{id}</div>
-      <div className={classes.name}>
-        <p>{name}</p>
-      </div>
-      <div className={classes.duration}>
-        <p>{duration}</p>
+    <div>
+      <div className={classes.album_item}>
+        <div>{id}</div>
+        <div className={classes.name}>
+          <p>{name}</p>
+        </div>
+        <div className={classes.duration}>
+          <p>{duration}</p>
+        </div>
       </div>
     </div>
   );
--- app/src/component/Album/SongsList.js
+++ app/src/component/Album/SongsList.js
@@ -1,11 +1,12 @@
 import React from "react";
-// import classes from "./SongsList.module.css";
+import classes from "./SongsList.module.css";
 
 import SongItem from "./SongItem";
 
 const SongsList = ({ items }) => {
   return (
     <ul>
+      <h2 className={classes.h2}>Songs:</h2>
       {items.map((item) => (
         <SongItem
           key={item.id}
